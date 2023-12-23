092765261000f5960337c820219684cf4a050f7f
otvegg <timian@husveg.net>
2023-12-23 13:55:32 +1100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,100 @@
-import * as React from "react"
+import * as React from 'react';
 import {
   ChakraProvider,
   Box,
+  Input,
   Text,
-  Link,
   VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Flex,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Coles } from './coles';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
+const MAXITEMS = 35;
+
+export const App = () => {
+  const [value, setValue] = React.useState('');
+  const [colesResponse, setColesResponse] = React.useState<any[]>([]);
+  const [wooliesResponse, setWooliesResponse] = React.useState<any[]>([]);
+
+  const getWooliesResponse = (link: string, term: string, pagenum: number) => {
+    axios
+      .get(link, {
+        params: {
+          searchTerm: term,
+          pageSize: MAXITEMS,
+          pageNumber: pagenum,
+        },
+      })
+      .then((response) => {
+        const amountReturned = response.data.Products.length;
+        setWooliesResponse([...wooliesResponse, ...response.data.Products]);
+
+        if (amountReturned === MAXITEMS && pagenum <= 3) getWooliesResponse(link, value, ++pagenum);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getColesResponse = (link: string, term: string, pagenum: number) => {
+    axios
+      .get(link, {
+        params: {
+          searchTerm: term,
+          pageSize: MAXITEMS,
+          pageNumber: pagenum,
+        },
+      })
+      .then((response) => {
+        const amountReturned = response.data.Products.length;
+        setWooliesResponse([...wooliesResponse, ...response.data.Products]);
+
+        if (amountReturned === MAXITEMS && pagenum <= 3) getWooliesResponse(link, value, ++pagenum);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  console.log(Coles());
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string> } }) => setValue(event.target.value);
+  const handleClick = () => {
+    setWooliesResponse([]);
+    //'https://shop.coles.com.au/online/COLRSSearchDisplay'
+    getWooliesResponse('https://www.woolworths.com.au/apis/ui/Search/products', value, 1);
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <VStack textAlign='center' fontSize='xl' w='100vw' h='100vh'>
+        <Flex w='full' justifyContent={'flex-end'}>
+          <ColorModeSwitcher justifySelf='flex-end' />
+        </Flex>
+
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <InputGroup>
+            <Input placeholder='Search for a product' variant='flushed' onChange={handleChange} value={value} />
+            <InputRightElement width='4.5rem'>
+              <Button h='1.75rem' size='sm' onClick={handleClick}>
+                {'Search'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+
+          {wooliesResponse.map((el, i) => (
+            <Text key={i}>
+              {el.DisplayName} - {el.Products[0].CupString}
+            </Text>
+          ))}
         </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+      </VStack>
+    </ChakraProvider>
+  );
+};
---
+++ src/coles.ts
@@ -0,0 +1,14 @@
+import { Builder, By } from 'selenium-webdriver';
+import edge from 'selenium-webdriver/edge';
+/*
+const service = new edge.ServiceBuilder(); 
+const driver = new Builder().forBrowser('edge').setEdgeService(service).build(); */
+
+export const Coles = async () => {
+  /*   await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+  let title = await driver.getTitle();
+  console.log(title);
+  await driver.manage().setTimeouts({ implicit: 500 });
+  let textBox = await driver.findElement(By.name('my-text'));
+  let submitButton = await driver.findElement(By.css('button')); */
+};
